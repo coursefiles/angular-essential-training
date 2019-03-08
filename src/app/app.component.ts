d493574ce1907c636e1b3bd851d48f0906bda223
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-08 11:57:48 -0800

    end of video commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  template: '<h1>My App</h1>'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {}
