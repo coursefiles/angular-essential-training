54a04cd5913286f7fb81b8499f0ae1a8b1fed5a3
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-08 12:15:08 -0800

    end of video commit

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {
-  name = 'The Redemption';
-
-  wasWatched() {
-    return true;
-  }
-}
+export class MediaItemComponent {}
