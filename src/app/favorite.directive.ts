98a532062174e6b58be150807cb55906fdd9b633
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-08 14:34:55 -0800

    end of video commit

---
+++ src/app/favorite.directive.ts
@@ -0,0 +1,8 @@
+import { Directive, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[mwFavorite]'
+})
+export class FavoriteDirective {
+  @HostBinding('class.is-favorite') isFavorite = true;
+}
