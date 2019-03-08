82bb325d0b72e7c84e994e2a0ddc1561d79c3070
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-08 14:34:55 -0800

    end of video commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { MediaItemListComponent } from './media-item-list.component';
   declarations: [
     AppComponent,
     MediaItemComponent,
-    MediaItemListComponent
+    MediaItemListComponent,
+    FavoriteDirective
   ],
   bootstrap: [
     AppComponent
