06d4ab6fd6b87410fde7fd1b9aa30431c7cef8fe
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-08 14:20:12 -0800

    end of video commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
 
 @NgModule({
   imports: [
@@ -9,7 +10,8 @@ import { MediaItemComponent } from './media-item.component';
   ],
   declarations: [
     AppComponent,
-    MediaItemComponent
+    MediaItemComponent,
+    MediaItemListComponent
   ],
   bootstrap: [
     AppComponent
