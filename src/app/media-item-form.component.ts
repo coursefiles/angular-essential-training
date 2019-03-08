f01037f01e4ea9c4f73189063a3d67c74517af73
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-08 14:52:31 -0800

    end of video commit

---
+++ src/app/media-item-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-media-item-form',
+  templateUrl: './media-item-form.component.html',
+  styleUrls: ['./media-item-form.component.css']
+})
+export class MediaItemFormComponent {}
