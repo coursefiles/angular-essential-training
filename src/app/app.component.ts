15c289299dbc9c1b00c3257a9bf347aaddcad0c9
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-08 14:20:12 -0800

    end of video commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  onMediaItemDelete(mediaItem) {
-    
-  }
-  
-  firstMediaItem = {
-    id: 1,
-    name: "Firebug",
-    medium: "Series",
-    category: "Science Fiction",
-    year: 2010,
-    watchedOn: 1294166565384,
-    isFavorite: false
-  };
-}
+export class AppComponent { }
