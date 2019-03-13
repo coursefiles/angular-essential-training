f4bc4f1611062b9c3e4fd58f0eb1af5e34c50fc5
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-13 08:44:55 -0700

    end of video commit

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -17,8 +17,22 @@ export class MediaItemFormComponent implements OnInit {
         Validators.pattern('[\\w\\-\\s\\/]+')
       ])),
       category: new FormControl(''),
-      year: new FormControl(''),
+      year: new FormControl('', this.yearValidator),
     });
+  }
+
+  yearValidator(control: FormControl) {
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+    const year = parseInt(control.value, 10);
+    const minYear = 1900;
+    const maxYear = 2100;
+    if (year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return { year: true };
+    }
   }
 
   onSubmit(mediaItem) {
