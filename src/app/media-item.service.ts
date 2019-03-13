5d1d61828faf9aac021286a50bc9fcd87e3639cc
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-13 10:29:47 -0700

    end of video commit

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -7,9 +7,9 @@ import { map } from 'rxjs/operators';
 })
 export class MediaItemService {
   constructor(private http: HttpClient) {}
-  
-  get(medium) {
-    let getOptions = {
+
+  get(medium: string) {
+    const getOptions = {
       params: { medium }
     };
     return this.http.get<MediaItemsResponse>('mediaitems', getOptions)
@@ -19,66 +19,18 @@ export class MediaItemService {
         })
       );
   }
-  
-  add(mediaItem) {
-    this.mediaItems.push(mediaItem);
-  }
-  
-  delete(mediaItem) {
-    let index = this.mediaItems.indexOf(mediaItem);
-    if(index >= 0) {
-      this.mediaItems.splice(index, 1);
-    }
+
+  add(mediaItem: MediaItem) {
+    return this.http.post('mediaitems', mediaItem);
   }
 
-  mediaItems = [
-    {
-      id: 1,
-      name: "Firebug",
-      medium: "Series",
-      category: "Science Fiction",
-      year: 2010,
-      watchedOn: 1294166565384,
-      isFavorite: false
-    },
-    {
-      id: 2,
-      name: "The Small Tall",
-      medium: "Movies",
-      category: "Comedy",
-      year: 2015,
-      watchedOn: null,
-      isFavorite: true
-    }, {
-      id: 3,
-      name: "The Redemption",
-      medium: "Movies",
-      category: "Action",
-      year: 2016,
-      watchedOn: null,
-      isFavorite: false
-    }, {
-      id: 4,
-      name: "Hoopers",
-      medium: "Series",
-      category: "Drama",
-      year: null,
-      watchedOn: null,
-      isFavorite: true
-    }, {
-      id: 5,
-      name: "Happy Joe: Cheery Road",
-      medium: "Movies",
-      category: "Action",
-      year: 2015,
-      watchedOn: 1457166565384,
-      isFavorite: false
-    }
-  ];
+  delete(mediaItem: MediaItem) {
+    return this.http.delete(`mediaitems/${mediaItem.id}`);
+  }
 }
 
 interface MediaItemsResponse {
-  mediaItems: MediaItem[]
+  mediaItems: MediaItem[];
 }
 
 export interface MediaItem {
