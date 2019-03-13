6738644030f37fb1d37d636ee0e380c375be633f
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-13 09:07:59 -0700

    end of video commit

---
+++ src/app/media-item.service.ts
@@ -0,0 +1,61 @@
+export class MediaItemService {
+  get() {
+    return this.mediaItems;
+  }
+  
+  add(mediaItem) {
+    this.mediaItems.push(mediaItem);
+  }
+  
+  delete(mediaItem) {
+    let index = this.mediaItems.indexOf(mediaItem);
+    if(index >= 0) {
+      this.mediaItems.splice(index, 1);
+    }
+  }
+
+  mediaItems = [
+    {
+      id: 1,
+      name: "Firebug",
+      medium: "Series",
+      category: "Science Fiction",
+      year: 2010,
+      watchedOn: 1294166565384,
+      isFavorite: false
+    },
+    {
+      id: 2,
+      name: "The Small Tall",
+      medium: "Movies",
+      category: "Comedy",
+      year: 2015,
+      watchedOn: null,
+      isFavorite: true
+    }, {
+      id: 3,
+      name: "The Redemption",
+      medium: "Movies",
+      category: "Action",
+      year: 2016,
+      watchedOn: null,
+      isFavorite: false
+    }, {
+      id: 4,
+      name: "Hoopers",
+      medium: "Series",
+      category: "Drama",
+      year: null,
+      watchedOn: null,
+      isFavorite: true
+    }, {
+      id: 5,
+      name: "Happy Joe: Cheery Road",
+      medium: "Movies",
+      category: "Action",
+      year: 2015,
+      watchedOn: 1457166565384,
+      isFavorite: false
+    }
+  ];
+}
