c0b1e3dfaf70a14da1936ab5aaa231977e7da9b7
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-13 09:25:25 -0700

    end of video commit

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MediaItemService } from './media-item.service';
 
@@ -12,7 +12,8 @@ export class MediaItemFormComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private mediaItemService: MediaItemService) {}
+    private mediaItemService: MediaItemService,
+    @Inject('lookupListToken') public lookupLists) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
