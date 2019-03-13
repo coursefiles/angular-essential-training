0c97fb375b2ac01341caf4a89a1d753b22cf0b75
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-13 10:10:41 -0700

    end of video commit

--- src/app/media-item-list.component.ts
+++ src/app/media-item-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MediaItemService } from './media-item.service';
+import { MediaItemService, MediaItem } from './media-item.service';
 
 @Component({
   selector: 'mw-media-item-list',
@@ -7,15 +7,24 @@ import { MediaItemService } from './media-item.service';
   styleUrls: ['./media-item-list.component.css']
 })
 export class MediaItemListComponent implements OnInit {
-  mediaItems;
+  medium = '';
+  mediaItems: MediaItem[];
 
   constructor(private mediaItemService: MediaItemService) {}
 
   ngOnInit() {
-    this.mediaItems = this.mediaItemService.get();
+    this.getMediaItems(this.medium);
   }
 
   onMediaItemDelete(mediaItem) {
     this.mediaItemService.delete(mediaItem);
+  }
+
+  getMediaItems(medium: string) {
+    this.medium = medium;
+    this.mediaItemService.get()
+      .subscribe(mediaItems => {
+        this.mediaItems = mediaItems;
+      });
   }
 }
