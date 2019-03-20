33974633d4e5173cab7dec434007428eeaab65b2
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-20 09:15:06 -0700

    end of video commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { MediaItemFormComponent } from './media-item-form.component';
+import { MediaItemListComponent } from './media-item-list.component';
+
+const appRoutes: Routes = [
+  { path: 'add', component: MediaItemFormComponent },
+  { path: ':medium', component: MediaItemListComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'all' }
+];
