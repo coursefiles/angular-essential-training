45c40aef1e5f87ade781f76a321567379a0b8326
Justin Schwartzenberger <schwarty@gmail.com>
2019-03-20 09:06:23 -0700

    end of video commit

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -1,36 +1,49 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MediaItemService {
   constructor(private http: HttpClient) {}
-  
+
   get(medium: string) {
-    let getOptions = {
+    const getOptions = {
       params: { medium }
     };
     return this.http.get<MediaItemsResponse>('mediaitems', getOptions)
       .pipe(
         map((response: MediaItemsResponse) => {
           return response.mediaItems;
-        })
+        }),
+        catchError(this.handleError)
       );
   }
-  
+
   add(mediaItem: MediaItem) {
-    return this.http.post('mediaitems', mediaItem);
+    return this.http.post('mediaitems', mediaItem)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
-  
+
   delete(mediaItem: MediaItem) {
-    return this.http.delete(`mediaitems/${mediaItem.id}`);
+    return this.http.delete(`mediaitems/${mediaItem.id}`)
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error(error.message);
+    return throwError('A data error occurred, please try again.');
   }
 }
 
 interface MediaItemsResponse {
-  mediaItems: MediaItem[]
+  mediaItems: MediaItem[];
 }
 
 export interface MediaItem {
