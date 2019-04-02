204db1b2bb79725bac8f05107b37363a78b7327d
Justin Schwartzenberger <schwarty@gmail.com>
2019-04-02 12:06:54 -0700

    end of video commit

--- src/app/category-list.component.ts
+++ src/app/category-list.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input } from "@angular/core";
-import { MediaItem } from "./media-item.service";
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: "category-list",
+  selector: 'mw-category-list',
   template: `
     <span class="label" *ngFor="let category of categories">
       {{ category }}
@@ -20,17 +19,10 @@ import { MediaItem } from "./media-item.service";
       :host-context(.medium-series) span {
         background-color: #45bf94;
       }
-      .label {
-        background-color: #999999;
-        color: #ffffff;
-        border-radius: 4px;
-        padding: 2px 6px;
-      }
       span {
         display: inline-block;
         margin-right: 4px;
         margin-bottom: 4px;
-        font-style: italic;
       }
     `
   ]
