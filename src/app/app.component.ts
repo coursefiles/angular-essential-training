8f4e6bc7ebe8154b07fe02e0e3979de27c0c2f02
vamsi <vamsea2002@hotmail.com>
2020-07-06 10:38:30 +0530

    Created-First-Component added boot strapping  code

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <p>this is my first angular app</p>
+    `,
+  styles: ['p {color:green}']
+
+})
+export class AppComponent { }
