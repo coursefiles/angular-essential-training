dc068ed63ea9ce0a404ffa45e5d6e467547e8305
Matthew Glenn <mfglenn@outlook.com>
2020-09-10 14:47:35 -0600

    Adjust the AppComponent to utilize the templateUrl metadata integration to access the HTML for the component from a supporting HTML file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'mw-app', //DOM Element selector for rendering
-    template: `
-        <h1>Media Watch App</h1>
-        <p>Keeping track of the media I want to watch.</p>
-    ` //HTML Template to be rendered
-})
+    templateUrl: './app.component.html'
+}) 
 export class AppComponent {}
