719a216cb401067bfb527fd71fa3ae2232500a84
Matthew Glenn <mfglenn@outlook.com>
2020-09-10 14:41:03 -0600

    Modify the selector syntax to meet the current angular spec of prefixing the DOM element selector with something pertaining to the app of feature.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root', //DOM Element selector for rendering
+    selector: 'mw-root', //DOM Element selector for rendering
     template: '<h1>Hello World</h1>' //HMTL Template to be rendered
 })
 export class AppComponent {}
