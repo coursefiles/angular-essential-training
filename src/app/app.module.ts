2c2f56eae416005345c4954aad416dbbbb3748fe
Matthew Glenn <mfglenn@outlook.com>
2020-09-10 15:24:12 -0600

    Update the AppModule to import the MediaItemComponent and include it in the AppModule declarations metadata for use in the app.  Update the app.component.html to incorporate the custom HTML tag for the MediaItemComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItemComponent
   ],
   bootstrap: [
     AppComponent
