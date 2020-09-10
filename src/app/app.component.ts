b8944c765df74fc288f86b73056d1f1186e6c080
Matthew Glenn <mfglenn@outlook.com>
2020-09-10 15:14:10 -0600

    Add the app.component.css file. Refactor the AppComponent styles to utilize the styleUrls metadata array to import the app.component.css file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'mw-app', //DOM Element selector for rendering
     templateUrl: './app.component.html',
-    styles: [
-        `
-        h1 { color: #ffffff; }
-        `,
-        `
-        .description { 
-            font-style: italic;
-            color: green; 
-        }
-        `
-    ] //Array of styles to be applied to the HTML provided through either the template or templateUrl metadata
+    styleUrls: ['./app.component.css'] //Array of style sheet URLs to be applied to the HTML provided through either the template or templateUrl metadata
 }) 
 export class AppComponent {}
