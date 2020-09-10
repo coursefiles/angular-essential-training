ab01e7ef7638cd20c6a4cabb48a41846e550c258
Matthew Glenn <mfglenn@outlook.com>
2020-09-10 15:30:03 -0600

    Adjust the MediaItemComponent to use interplation to inject prperty values and evaluate methods to modifiy the content of the HTML.

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { TimeoutError } from 'rxjs';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  name='The Redemption';
+
+  wasWatched() {
+    return true
+  };
+}
