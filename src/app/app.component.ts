5043e4236a131bc5e38e7a418811a919b2eb69f1
Matthew Glenn <mfglenn@outlook.com>
2020-09-10 14:45:35 -0600

    Update the template metadata property of the AppComponent decorator with to user template string syntax for readability.  Add description for the app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'mw-app', //DOM Element selector for rendering
-    template: '<h1>Hello World</h1>' //HMTL Template to be rendered
+    template: `
+        <h1>Media Watch App</h1>
+        <p>Keeping track of the media I want to watch.</p>
+    ` //HTML Template to be rendered
 })
 export class AppComponent {}
