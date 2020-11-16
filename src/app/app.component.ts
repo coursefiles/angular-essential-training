02f99326b6200bb285a469e56c9b6bf5e8471f94
Tristan Pacba <tpacba@live.com>
2020-11-16 13:40:44 -0800

    added ngif for watchedon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
     medium: 'Series',
     category: 'Science Fiction',
     year: 2010,
-    watchedOn: 1294166565384,
+    watchedOn: null,
     isFavorite: false
   };
 
