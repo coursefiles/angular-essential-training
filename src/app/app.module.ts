476f7e2256e54c1523418506ea925fa0b820619c
zoegagnon <zgagn033@uottawa.ca>
2022-05-11 16:30:02 -0400

    changes for Component section of course

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent,
+        MediaItemComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule {}
